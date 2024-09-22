2425490144c07587045782923094cd6b76329538
vipracitti7 <vipracitti7@gmail.com>
2024-09-22 03:38:10 +0000

    Set project goals

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,10 @@
 console.log("Vipracitti")
 console.log ("test")
+/* Why are you learning Js?
+Because i want to build products like : 
+A time management web.
+Social Accountability web for govt and people.
+Swacch Bharat web
+Daily wages web ( where all unorganised work posted and daily labourers are available to hire same as freelancer in educated world )
+etc.
+*/
